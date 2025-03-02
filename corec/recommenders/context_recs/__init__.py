85f3dda81aee95207bf15d460812af3f6a719422
Gimillo <jaimegimillo@gmail.com>
2025-03-02 19:04:21 +0100

    chore: remove legacy dirs

--- corec/recommenders/context_recs/__init__.py
+++
@@ -1,9 +0,0 @@
-from .heuristic_recs.context_pop import ContextPopRec
-from .heuristic_recs.context_random import ContextRandomRec
-from .heuristic_recs.context_satisfaction import ContextSatisfactionRec
-
-__all__ = [
-    "ContextPopRec",
-    "ContextRandomRec",
-    "ContextSatisfactionRec",
-]
